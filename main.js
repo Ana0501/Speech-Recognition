73c63bab6f09321926f0f0c7e1264db7bab2538c
Ana0501 <61860214+Ana0501@users.noreply.github.com>
2022-03-14 21:09:13 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,51 +1,48 @@
-const texts = document.querySelector(".texts");
+const texts = document.querySelector('.texts');
 
-window.SpeechRecognition =
-  window.SpeechRecognition || window.webkitSpeechRecognition;
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
 recognition.interimResults = true;
 
-let p = document.createElement("p");
+let p = document.createElement('p');
 
-recognition.addEventListener("result", (e) => {
+recognition.addEventListener('result', (e)=>{
   texts.appendChild(p);
   const text = Array.from(e.results)
-    .map((result) => result[0])
-    .map((result) => result.transcript)
-    .join("");
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
 
   p.innerText = text;
-  if (e.results[0].isFinal) {
-    if (text.includes("how are you")) {
-      p = document.createElement("p");
-      p.classList.add("replay");
-      p.innerText = "I am fine";
-      texts.appendChild(p);
+  if(e.results[0].isFinal){
+    if (text.includes('¿Cómo estás?')) {
+      p = document.createElement('p');
+      p.classList.add('replay');
+      p.innerText = 'Estoy bien';
+      texts.appendChild(p)
     }
-    if (
-      text.includes("what's your name") ||
-      text.includes("what is your name")
-    ) {
-      p = document.createElement("p");
-      p.classList.add("replay");
-      p.innerText = "My Name is Cifar";
-      texts.appendChild(p);
+    if (text.includes("Cómo te llamas") || text.includes('Cómo te llamas')) {
+      p = document.createElement('p');
+      p.classList.add('replay');
+      p.innerText = 'Mi nombre es Ana';
+      texts.appendChild(p)
     }
-    if (text.includes("open my YouTube")) {
-      p = document.createElement("p");
-      p.classList.add("replay");
-      p.innerText = "opening youtube channel";
-      texts.appendChild(p);
-      console.log("opening youtube");
-      window.open("https://www.youtube.com/channel/UCdxaLo9ALJgXgOUDURRPGiQ");
+    if (text.includes('open my YouTube')) {
+      p = document.createElement('p');
+      p.classList.add('replay');
+      p.innerText = 'opening youtube channel';
+      texts.appendChild(p)
+      console.log('opening youtube')
+      window.open('https://www.youtube.com/channel/UCdxaLo9ALJgXgOUDURRPGiQ')
     }
-    p = document.createElement("p");
+    p = document.createElement('p');
   }
 });
 
-recognition.addEventListener("end", () => {
+
+recognition.addEventListener('end', ()=>{
   recognition.start();
-});
+})
 
 recognition.start();
